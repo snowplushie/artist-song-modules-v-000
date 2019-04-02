a4db9c17777a490000920d7d63e6b6be552a20af
Paige Wiener <snowplushie@yahoo.com>
2019-04-02 01:08:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,12 @@
 require 'pry'
 
 class Song
+  
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
   attr_accessor :name
   attr_reader :artist
   
